8044555665f19973f0dfb0d0f68e25065df0a877
darsh-chavda <dhchavda1931@gmail.com>
2024-03-31 12:21:28 +0530

    removed multiple environment json files

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,17 @@
 import { expect, test } from "@playwright/test";
-import * as devData from "../data/dev-data.json";
-import * as stgData from "../data/stg-data.json";
-import * as prodData from "../data/prod-data.json";
+import * as data from "../data/data.json";
 
-test.describe('An example suite of test data for multiple environments', () => {
+test.describe("An example suite of test data for multiple environments", () => {
+  const env: string = (process.env.ENV || "dev").toLowerCase();
+  const testData = data[env];
 
-  let testData: any;
-  process.env.ENV === 'stg' ? testData = stgData : process.env.ENV === 'prod' ? testData = prodData : testData = devData;
-
-  test('An example test case of test data for multiple environments', async ({ page }) => {
-
+  test("An example test case of test data for multiple environments", async ({ page }) => {
     // Read data from .env files
-    console.log(process.env.ENV);
+    console.log(env);
     console.log(process.env.BASE_URL);
 
-    // Read testData from .json files 
+    // Read testData from .json files
     console.log(testData.userName);
     console.log(testData.password);
-  })
-})
+  });
+});
